5f51f94a2b50e6b230daa059c82a0b9f6362146e
mdsaibhossain <saibhossain5@gmail.com>
2025-08-25 21:05:03 +0600

    pdf

--- extractPdf.py
+++ extractPdf.py
@@ -1,6 +1,7 @@
 import fitz
-import nltk
-from nltk.tokenize import sent_tokenize
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
 
 def extract_text_from_pdf(pdf_path):
     text = ""
@@ -10,4 +11,11 @@ def extract_text_from_pdf(pdf_path):
     return text
 
 # pdf_text = extract_text_from_pdf("AdapterSwap: Continuous Training of LLMs with Data Removal and Access-Control Guarantees.pdf")
-# print(pdf_text[100:500])
+# print(pdf_text[0:500])
+
+def split_sentences(text):
+    doc = nlp(text)
+    return [sent.text for sent in doc.sents]
+
+# sentence = split_sentences(pdf_text)
+# print("-----",sentence[:5])
